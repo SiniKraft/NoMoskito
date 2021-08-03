a9e03d4349e2991e5713db914b14521988c85ca8
SiniKraft <natclaudix@gmail.com>
2021-08-03 13:27:12 +0200

    Update and bug fixes

---
+++ server/server.py
@@ -0,0 +1,51 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import os
+
+def recreate_conf():
+    with open("server.ini", "w+") as file:
+        file.write("host_name=\"localhost\"\nport=8080")
+
+try:
+    os.rename("server.ini", "serverini.py")
+    try:
+        from serverini import *
+    except:
+        recreate_conf()
+    os.rename("serverini.py", "server.ini")
+except:
+    recreate_conf()
+
+try:
+    os.removedirs("__pycache__")
+except:
+    pass
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)  # 200 = successful
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((host_name, port), MyServer)
+    print("Server started http://%s:%s" % (host_name, port))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+
+try:
+    os.removedirs("__pycache__")
+except:
+    pass
